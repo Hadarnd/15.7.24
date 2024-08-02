79e02b19cd64a569bec27872d75951bdd5b77904
Hadar <hadarnd8@gmail.com>
2024-08-02 12:48:01 +0300

    homeworj5.2.py

--- homeworj5.2.py
+++ homeworj5.2.py
@@ -3,8 +3,46 @@
 sum = 0
 sum: int = int(input("numnber:"))
 
-for i in range(1,101):
-    if not i % 7:
+#start
+
+import random
+count = 0
+yes = 0
+no = 0
+for i in range(1):
+    rnd: int = random.randint(1, 101)
+    print("The random number is:", end=" ")
+    print(rnd, end=" ")
+    print()
+
+while True:
+
+    num: int = int(input("Welcome to a loto game please choose a number:"))
+    count += 1
+    if num == rnd:
+        print(f"BINGO!!")
+        break
+
+    if num > rnd:
+        print(f"number is too big")
+    if num < rnd:
+        print(f"number is too small")
+
+    print(f"Numbers of tring:{count}")
+    choice = input("Would you like to continue the game? (yes/ no):")
+    if choice.casefold() == no:
+        break
+
+
+
+
+
+
+
+
+
+
+#end    if not i % 7:
         continue
     sum += 1
     print(i);
